a06c681285efc3918e1f2056e6426f4e8a986466
Obliviated <hamzacoskun41@gmail.com>
2022-07-28 10:08:01 +0300

    Initial commit

---
+++ placeholder/src/main/java/mc/obliviate/util/placeholder/InternalPlaceholder.java
@@ -0,0 +1,21 @@
+package mc.obliviate.util.placeholder;
+
+public class InternalPlaceholder {
+
+	private final String placeholder;
+	private final String value;
+
+	protected InternalPlaceholder(final String placeholder, final String value) {
+		this.placeholder = placeholder;
+		this.value = value;
+	}
+
+	public String getPlaceholder() {
+		return placeholder;
+	}
+
+	public String getValue() {
+		if (value == null) return "";
+		return value;
+	}
+}
