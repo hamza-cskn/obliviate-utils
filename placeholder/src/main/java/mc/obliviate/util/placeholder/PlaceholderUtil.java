392ffcd0d1b90ea28fc6953d9fff3ff436d035e0
Obliviated <hamzacoskun41@gmail.com>
2022-08-24 16:47:28 +0300

    document: add more document

--- placeholder/src/main/java/mc/obliviate/util/placeholder/PlaceholderUtil.java
+++ placeholder/src/main/java/mc/obliviate/util/placeholder/PlaceholderUtil.java
@@ -7,6 +7,15 @@ public class PlaceholderUtil {
 
 	private final List<InternalPlaceholder> placeholders = new ArrayList<>();
 
+	/**
+	 *
+	 * Registers new placeholder (key, value relation) to
+	 * the placeholder util.
+	 *
+	 * @param key pseudo string, search string - ex: {player-name}
+	 * @param value result of placeholder, replace string, - ex: Mr_Obliviate
+	 * @return
+	 */
 	public PlaceholderUtil add(final String key, final String value) {
 		placeholders.add(new InternalPlaceholder(key, value));
 		return this;
@@ -16,7 +25,22 @@ public class PlaceholderUtil {
 		return placeholders;
 	}
 
-	public List<String> apply(final List<String> texts) {
+	/**
+	 * Adds all placeholders and their values of param placeholder util
+	 * to this placeholder util.
+	 * @param placeholderUtil
+	 */
+	public void merge(PlaceholderUtil placeholderUtil) {
+		this.placeholders.addAll(new ArrayList<>(placeholderUtil.placeholders));
+	}
+
+	/**
+	 * Parses placeholders of all String
+	 * values of list
+	 *
+	 * @param texts
+	 */
+	public List<String> apply(final Iterable<String> texts) {
 		final List<String> result = new ArrayList<>();
 		for (final String text : texts) {
 			result.add(apply(text));
@@ -24,6 +48,12 @@ public class PlaceholderUtil {
 		return result;
 	}
 
+	/**
+	 * Parses placeholders of String
+	 * value
+	 *
+	 * @param text
+	 */
 	public String apply(String text) {
 		for (final InternalPlaceholder placeholder : placeholders) {
 			text = text.replace(placeholder.getPlaceholder(), placeholder.getValue());
