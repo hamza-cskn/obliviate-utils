383f59cde8daf220bd5c26522864ed6a67733ea1
Obliviated <hamzacoskun41@gmail.com>
2022-08-27 11:20:22 +0300

    feat: add static apply method.

--- placeholder/src/main/java/mc/obliviate/util/placeholder/PlaceholderUtil.java
+++ placeholder/src/main/java/mc/obliviate/util/placeholder/PlaceholderUtil.java
@@ -7,61 +7,77 @@ import java.util.Set;
 
 public class PlaceholderUtil {
 
-	private final Set<InternalPlaceholder> placeholders = new HashSet<>();
+    private final Set<InternalPlaceholder> placeholders = new HashSet<>();
 
-	/**
-	 *
-	 * Registers new placeholder (key, value relation) to
-	 * the placeholder util.
-	 *
-	 * @param key pseudo string, search string - ex: {player-name}
-	 * @param value result of placeholder, replace string, - ex: Mr_Obliviate
-	 * @return
-	 */
-	public PlaceholderUtil add(final String key, final String value) {
-		placeholders.add(new InternalPlaceholder(key, value));
-		return this;
-	}
+    /**
+     * Registers new placeholder (key, value relation) to
+     * the placeholder util.
+     *
+     * @param key   pseudo string, search string - ex: {player-name}
+     * @param value result of placeholder, replace string, - ex: Mr_Obliviate
+     * @return
+     */
+    public PlaceholderUtil add(final String key, final String value) {
+        placeholders.add(new InternalPlaceholder(key, value));
+        return this;
+    }
 
-	public Set<InternalPlaceholder> getPlaceholders() {
-		return placeholders;
-	}
+    public Set<InternalPlaceholder> getPlaceholders() {
+        return placeholders;
+    }
 
-	/**
-	 * Adds all placeholders and their values of param placeholder util
-	 * to this placeholder util.
-	 * @param placeholderUtil
-	 */
-	public void merge(PlaceholderUtil placeholderUtil) {
-		this.placeholders.addAll(new ArrayList<>(placeholderUtil.placeholders));
-	}
+    /**
+     * Adds all placeholders and their values of param placeholder util
+     * to this placeholder util.
+     *
+     * @param placeholderUtil
+     */
+    public void merge(PlaceholderUtil placeholderUtil) {
+        this.placeholders.addAll(new ArrayList<>(placeholderUtil.placeholders));
+    }
 
-	/**
-	 * Parses placeholders of all String
-	 * values of list
-	 *
-	 * @param texts
-	 */
-	public List<String> apply(final Iterable<String> texts) {
-		final List<String> result = new ArrayList<>();
-		for (final String text : texts) {
-			result.add(apply(text));
-		}
-		return result;
-	}
+    /**
+     * Parses placeholders of all String
+     * values of list
+     *
+     * @param texts texts to be parsed
+     */
+    public List<String> apply(final Iterable<String> texts) {
+        final List<String> result = new ArrayList<>();
+        for (final String text : texts) {
+            result.add(apply(text));
+        }
+        return result;
+    }
 
-	/**
-	 * Parses placeholders of String
-	 * value
-	 *
-	 * @param text
-	 */
-	public String apply(String text) {
-		for (final InternalPlaceholder placeholder : placeholders) {
-			text = text.replace(placeholder.getPlaceholder(), placeholder.getValue());
-		}
-		return text;
-	}
+    /**
+     * Parses placeholders of String
+     * value
+     *
+     * @param text text to be parsed
+     */
+    public String apply(String text) {
+        for (final InternalPlaceholder placeholder : placeholders) {
+            text = text.replace(placeholder.getPlaceholder(), placeholder.getValue());
+        }
+        return text;
+    }
+
+    public static String apply(String text, PlaceholderUtil placeholderUtil) {
+        if (placeholderUtil == null) return text;
+        return placeholderUtil.apply(text);
+    }
+
+    public static List<String> apply(final Iterable<String> texts, PlaceholderUtil placeholderUtil) {
+        if (placeholderUtil == null) {
+            if (texts instanceof List) return (List<String>) texts;
+
+            final List<String> list = new ArrayList<>();
+            for (String text : texts) list.add(text);
+            return list;
+        }
+        return placeholderUtil.apply(texts);
+    }
 
 
 }
